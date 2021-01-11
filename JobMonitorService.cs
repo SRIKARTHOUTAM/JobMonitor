54724eb0455bc34619e82ad0dc2f15b2040bc8bf
SRIKAR <SRIKARTHOUTAM@GMAIL.COM>
2021-01-10 19:22:11 -0500

    Add files via upload

---
+++ JobMonitorService.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace JobMonitor
+{
+    public partial class JobMonitorService : ServiceBase
+    {
+        private Timer timer;
+        string batchFilePath = string.Empty;
+        string programName = string.Empty;
+        string logFilePath = string.Empty;
+        public JobMonitorService(string[] args)
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            var appRunFrequency = Convert.ToInt32(ConfigurationManager.AppSettings["RunFrequencyInMins"]);
+            //AuditLog FilePath
+            logFilePath = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location), $"JobMonitorAuditLog_{DateTime.Now.ToString("MM_dd_yyyy")}.txt");
+            string localMachineName = Environment.MachineName;
+            Process p = Process.GetProcesses(localMachineName).Where(c => c.ProcessName == "JobMonitor").FirstOrDefault();
+            using (StreamWriter log = new StreamWriter(logFilePath, append: true))
+            {
+                log.Write("********************Job Monitor*******************" + Environment.NewLine);
+                log.Write($"Info: Job Monitor Service Started at {DateTime.Now}, and runs at every {appRunFrequency}mins interval." + Environment.NewLine);
+
+                if (args.Length == 2)
+                {
+                    batchFilePath = args[0];
+                    EventLog.WriteEntry($"First Parameter Received BatchFilePath: {batchFilePath}.");
+                    programName = args[1];
+                    EventLog.WriteEntry($"Second Parameter Received Monitoring ProgramName: {programName}.");
+                    if (File.Exists(batchFilePath))
+                    {
+                        log.Write($"Info: BatchFile Location: {batchFilePath}." + Environment.NewLine);
+                        log.Write($"Info: Monitoring Program Name: {programName}." + Environment.NewLine);
+                    }
+                    else
+                    {
+                        log.Write($"Warning: BatchFile not exists in the given location: {batchFilePath} ." + Environment.NewLine);
+                        p.Kill();
+                    }
+                    timer = new Timer(appRunFrequency * 60 * 1000);
+                    timer.Elapsed += new ElapsedEventHandler(MonitorApps);
+                    timer.Start();
+                }
+                else
+                {
+                    log.Write($"Error: Expected BatchFilePath and ProgramName not passed with Application start. Stopping the Job Monitor service." + Environment.NewLine);
+                    p.Kill();
+                }
+            }
+        }
+
+        private void MonitorApps(object sender, ElapsedEventArgs e)
+        {
+            using (StreamWriter log = new StreamWriter(logFilePath, append: true))
+            {
+                try
+                {
+                    string localMachineName = Environment.MachineName;
+                    var isProgramRunning = Process.GetProcesses(localMachineName).Where(c => c.ProcessName == programName).Any();
+                    if (isProgramRunning)
+                    {
+                        log.Write($"Info: {DateTime.Now}; {programName} is Up and Running." + Environment.NewLine);
+                    }
+                    else
+                    {
+                        log.Write($"Info: {DateTime.Now}; {programName} is not running, Triggering Bat file at location {batchFilePath}. " + Environment.NewLine);
+                        var startInfo = new ProcessStartInfo
+                        {
+                            FileName = "cmd.exe",
+                            Arguments = "/C " + batchFilePath,
+                            CreateNoWindow = true,
+                            ErrorDialog = false,
+                            RedirectStandardError = true,
+                            RedirectStandardOutput = true,
+                            UseShellExecute = false,
+                            WindowStyle = ProcessWindowStyle.Hidden
+                        };
+                        startInfo.EnvironmentVariables.Add("CATALINA_HOME", Path.GetDirectoryName(batchFilePath));
+                        var process = new Process();
+                        process.StartInfo = startInfo;
+                        process.Start();
+                        log.Write($"Info: {DateTime.Now} Bat File Triggered." + Environment.NewLine);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    log.Write($"Error: Exception Occured. Error Message: {ex.Message}; InnerException: {ex.InnerException}." + Environment.NewLine);
+                }
+            }
+        }
+
+        protected override void OnStop()
+        {
+            using (StreamWriter log = new StreamWriter(logFilePath, append: true))
+            {
+                log.Write($"Warning: Service Stopped at {DateTime.Now}." + Environment.NewLine);
+            }
+            timer.Stop();
+            timer.Dispose();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobMonitor
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            var logFilePath = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location), $"JobMonitorAuditLog_{DateTime.Now.ToString("MM/dd/yyyy")}.txt");
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new JobMonitorService(args)
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
